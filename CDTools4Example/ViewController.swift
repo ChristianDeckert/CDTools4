9cef0274d7fc83c023b06db2eb737e4e902dfe9b
Christian Deckert <christian.deckert@icloud.com>
2018-03-15 17:01:39 +0100

    Added podspec. Added a demo target (to be filled with life)

---
+++ CDTools4Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CDTools4Example
+//
+//  Created by Christian on 15.03.18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
