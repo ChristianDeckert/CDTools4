a8122b34daa6cbb283480a4d06216852a29743f1
Christian Deckert <deckert.christian@icloud.com>
2018-09-11 16:08:29 +0200

    Updated Haptics.swift

--- Foundation/Haptics.swift
+++ Foundation/Haptics.swift
@@ -3,45 +3,35 @@ import Foundation
 import AudioToolbox
 
 public struct Haptics {
-    
-    public static func softTap(fallbackVibration: Bool = false) {
-        if #available(iOS 10.0, *) {
-            let generator = UISelectionFeedbackGenerator()
-            generator.prepare()
-            generator.selectionChanged()
-        } else if fallbackVibration {
-            // Fallback on earlier versions
-            vibrate()
-        }
-        
+  
+  public static func softTap() {
+    if #available(iOS 10.0, *) {
+      let generator = UISelectionFeedbackGenerator()
+      generator.prepare()
+      generator.selectionChanged()
     }
-    
-    public static func doubleTap(type: UINotificationFeedbackGenerator.FeedbackType = .warning, fallbackVibration: Bool = false) {
-        if #available(iOS 10.0, *) {
-            let generator = UINotificationFeedbackGenerator()
-            generator.prepare()
-            generator.notificationOccurred(type)
-        } else if fallbackVibration {
-            // Fallback on earlier versions
-            vibrate()
-        }
-        
+  }
+  
+  public static func doubleTap() {
+    if #available(iOS 10.0, *) {
+      let generator = UINotificationFeedbackGenerator()
+      generator.prepare()
+      generator.notificationOccurred(.warning)
     }
-    
-    // a single tap via taptic engine
-    public static func singleTap(style: UIImpactFeedbackGenerator.FeedbackStyle = .heavy, fallbackVibration: Bool = false) {
-        if #available(iOS 10.0, *) {
-            let generator = UIImpactFeedbackGenerator(style: style)
-            generator.prepare()
-            generator.impactOccurred()
-        } else if fallbackVibration {
-            // Fallback on earlier versions
-            vibrate()
-        }
-        
+  }
+  
+  // a single tap via taptic engine
+  public static func singleTap() {
+    if #available(iOS 10.0, *) {
+      let generator = UIImpactFeedbackGenerator(style: .heavy)
+      generator.prepare()
+      generator.impactOccurred()
     }
-    
-    public static func vibrate() {
-        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-    }
+  }
+  
+  public static func vibrate() {
+    AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+  }
 }
+
+
